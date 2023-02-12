cd35bae0a8267813e4e00765f21b32dde12c9087
0xfsk <furkankulaksiz68@gmail.com>
2023-02-12 19:53:57 +0300

    LeetCode 342 solution

---
+++ src/main/java/com/fsk/Main20.java
@@ -0,0 +1,26 @@
+package com.fsk;
+
+/**
+ * LeetCode -> 342. Power of Four
+ */
+
+public class Main20 {
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(16));
+    }
+
+    public static boolean isPowerOfFour(int n) {
+        if (n < 1) {
+            return false;
+        }
+
+        while (n % 4 == 0) {
+            n = n / 4;
+        }
+
+        if (n == 1) {
+            return true;
+        }else return false;
+    }
+}
